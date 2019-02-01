c12436ad3205eba6505a3f91f9f565241bac4320
DaniAkash <dani@pickyourtrail.com>
2019-02-01 18:41:38 +0530

    create date picker - UI only

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -3,24 +3,103 @@ import React, {
 } from 'react';
 import {
     View,
+    TextInput,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
 } from 'react-native';
 
 class RegistrationScreen extends Component {
 
-    static navigationOptions = ({navigation}) => {
+    static navigationOptions = ({ navigation }) => {
         const title = navigation.getParam('title', 'Default title');
         return {
             title,
         };
     };
 
+    state = {
+        name: "",
+        email: "",
+        mobileNumber: "",
+        dateOfBirth: new Date(1990, 1, 1),
+    };
+    _nameRef = React.createRef();
+    _emailRef = React.createRef();
+    _mobileRef = React.createRef();
+
     render() {
         return (
-            <View>
-                
+            <View style={styles.registrationContainer}>
+                <TextInput
+                    ref={this._nameRef}
+                    placeholder={'Your Name...'}
+                    onChangeText={name => this.setState({ name })}
+                    underlineColorAndroid={'black'}
+                    value={this.state.name}
+                    returnKeyType={'next'}
+                    onSubmitEditing={() => {
+                        this._emailRef.current.focus();
+                    }}
+                />
+
+                <TextInput
+                    ref={this._emailRef}
+                    placeholder={'Your Email...'}
+                    onChangeText={email => this.setState({ email })}
+                    underlineColorAndroid={'black'}
+                    value={this.state.email}
+                    returnKeyType={'next'}
+                    keyboardType={'email-address'}
+                    onSubmitEditing={() => {
+                        this._mobileRef.current.focus();
+                    }}
+                />
+
+                <TextInput
+                    ref={this._mobileRef}
+                    placeholder={'Your Mobile Number...'}
+                    onChangeText={mobileNumber => this.setState({ mobileNumber })}
+                    underlineColorAndroid={'black'}
+                    value={this.state.mobileNumber}
+                    returnKeyType={'next'}
+                    keyboardType={'phone-pad'}
+                    onSubmitEditing={() => {
+
+                    }}
+                />
+
+                <TouchableHighlight
+                    underlayColor={'transparent'}
+                    style={{ height: 50 }}
+                    onPress={() => this.openDatePicker()}
+                >
+                    <View style={styles.dateContainer}>
+                        <Text>Date of Birth:</Text>
+                        <Text>{`${this.state.dateOfBirth.getDate()}/${
+                            this.state.dateOfBirth.getMonth()
+                            }/${this.state.dateOfBirth.getFullYear()}`}</Text>
+                    </View>
+                </TouchableHighlight>
+
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    registrationContainer: {
+        flex: 1,
+        margin: 10,
+    },
+    dateContainer: { 
+        height: 35, 
+        backgroundColor: 'silver', 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-around', 
+        marginVertical: 10 
+    }
+})
 
 export default RegistrationScreen;
