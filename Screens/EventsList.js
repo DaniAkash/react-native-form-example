989d0bd03611066717c369b07465f482b1e9629b
DaniAkash <dani@pickyourtrail.com>
2019-02-01 17:15:33 +0530

    add styles to events list

--- Screens/EventsList.js
+++ Screens/EventsList.js
@@ -4,20 +4,76 @@ import React, {
 import {
     View,
     FlatList,
-    Text
+    Text,
+    TouchableHighlight,
+    StyleSheet
 } from 'react-native';
 import events from '../data/events';
 
 // https://pastebin.com/raw/vcxBNZYE
 class EventsList extends Component {
+
+    _renderRow = ({item, index}) => {
+        return (
+            <TouchableHighlight 
+                style={styles.rowTouchable}
+                underlayColor={'silver'}
+                onPress={() => null}
+                >
+                <View style={[styles.rowContainer, {
+                    backgroundColor: index %2 ? '#F0FFF0': 'white'
+                }]}>
+                    <Text style={styles.eventNameText}>{item.event}</Text>
+                    <View style={styles.eventDetailsContainer}>
+                        <Text style={styles.locationText}>{item.location}</Text>
+                        <Text style={styles.dateText}>{item.date}</Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        );
+    };
+
+    _keyExtractor = (item, index) => item.id;
+
+    _renderSeperator = () => {
+        return(
+            <View style={{backgroundColor: 'black', height: StyleSheet.hairlineWidth}}/>
+        );
+    }
+
     render() {
         return (
             <FlatList
                 data={events}
-                renderItem={({item}) => <Text>{item.event}</Text>}
+                renderItem={this._renderRow}
+                keyExtractor={this._keyExtractor}
+                ItemSeparatorComponent={this._renderSeperator}
             />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    rowTouchable: {
+
+    },
+    rowContainer: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+    },
+    eventNameText: {
+        marginBottom: 10,
+    },
+    eventDetailsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    locationText: {
+
+    },
+    location: {
+
+    },
+});
 
 export default EventsList;
